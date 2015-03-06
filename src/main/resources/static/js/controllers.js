e6991f4bc389852293fb754ccde018d3bb5a17d4
anilkd <anilkd@gmail.com>
2015-03-06 22:18:23 +0000

    cancel function

--- src/main/resources/static/js/controllers.js
+++ src/main/resources/static/js/controllers.js
@@ -1,44 +1,42 @@
 angular.module('BookStoreApp.controllers', []).
-    controller('BookStoreController', function ($state, $stateParams, $scope, BookStoreAPIService) {
+    controller('BookStoreController', function ($state, $rootScope, $stateParams, $scope, BookStoreAPIService) {
         BookStoreAPIService.query(function (data) {
             $scope.bookList = data
 
         })
+        $rootScope.cancel = function () {
 
-        $scope.deleteBook = function (book) {
-            BookStoreAPIService.remove({bookId: book.bookId})
+            $rootScope.mainView()
+        }
+
+        $rootScope.mainView = function () {
             $state.transitionTo('books', $stateParams, {
                 reload: true,
                 inherit: false,
                 notify: true
             });
+
+        }
+        $scope.deleteBook = function (book) {
+            BookStoreAPIService.remove({bookId: book.bookId})
+            $rootScope.mainView()
         }
     })
-    .controller('addBookController', function ($state, $stateParams, $scope, BookStoreAPIService) {
+    .controller('addBookController', function ($state,$rootScope, $stateParams, $scope, BookStoreAPIService) {
 
         $scope.addBook = function (book) {
             BookStoreAPIService.save(book)
-            $state.transitionTo('books', $stateParams, {
-                reload: true,
-                inherit: false,
-                notify: true
-            });
+            $rootScope.mainView()
         }
     })
 
-    .controller('editBookController', function ($scope, $state, $stateParams, BookStoreAPIService) {
+    .controller('editBookController', function ($scope,$rootScope, $state, $stateParams, BookStoreAPIService) {
 
-        console.log($stateParams.bookId);
         $scope.currentEditBook = BookStoreAPIService.get({bookId: $stateParams.bookId});
-        console.log($scope.currentEditBook)
         $scope.updateBook = function (book) {
             console.log(book)
             BookStoreAPIService.updateBook(book)
-            $state.transitionTo('books', $stateParams, {
-                reload: true,
-                inherit: false,
-                notify: true
-            });
+            $rootScope.mainView()
         }
 
 
